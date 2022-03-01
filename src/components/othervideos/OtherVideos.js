4ee74beed34d2efed956cd5c0efb3db62f63d169
Hasib Hussain <hasibh213@gmail.com>
2022-03-01 03:13:53 +0000

    half css done

--- src/components/othervideos/OtherVideos.js
+++ src/components/othervideos/OtherVideos.js
@@ -3,30 +3,32 @@ import "./OtherVideos.scss";
 function OtherVideos(props) {
   return (
     <>
-      Next Videos
-      {props.videos
-        .filter((v) => v.id != props.specificVideo.id)
-        .map((otherVideo) => (
-          <div
-            className="othervideos"
-            key={otherVideo.id}
-            onClick={() => props.onVideoClick(otherVideo.id)}
-          >
-            <div className="othervideos__header">
-              <article className="othervideos__card">
-                <img
-                  className="othervideos__image"
-                  src={otherVideo.image}
-                  alt="video-image"
-                />
-                <div className="othervideos__text">
-                  <h3 className="othervideos__title">{otherVideo.title}</h3>
-                  <p className="othervideos__channel">{otherVideo.channel}</p>
-                </div>
-              </article>
+      <div className="othervideos__header">
+        Next Videos
+        {props.videos
+          .filter((v) => v.id != props.specificVideo.id)
+          .map((otherVideo) => (
+            <div
+              className="othervideos"
+              key={otherVideo.id}
+              onClick={() => props.onVideoClick(otherVideo.id)}
+            >
+              <div className="othervideos__header">
+                <article className="othervideos__card">
+                  <img
+                    className="othervideos__image"
+                    src={otherVideo.image}
+                    alt="video-image"
+                  />
+                  <div className="othervideos__text">
+                    <h3 className="othervideos__title">{otherVideo.title}</h3>
+                    <p className="othervideos__channel">{otherVideo.channel}</p>
+                  </div>
+                </article>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+      </div>
     </>
   );
 }
